ff704f147c6c315b21532bfe4d1be01ed17e029a
KevinLi0711 <108951684+Luominai@users.noreply.github.com>
2024-05-26 18:21:27 -0400

    figuring out sql.js

--- src/PokemonTable.tsx
+++ src/PokemonTable.tsx
@@ -1,8 +1,45 @@
 import { Table, TableCell, TableHead, TableRow, Typography } from "@mui/material"
 import Pokemon from "./Pokemon"
 import settings from "./settings.json"
+import initSqlJs, { Database } from "sql.js"
+import { useEffect, useState } from "react";
 
-function PokemonTable({listOfPokemon}: {listOfPokemon: Array<Pokemon>}) {
+
+function query({filters}: {filters: {name: string, type: string, ability: string, move: string}}) {
+    
+}
+
+function PokemonTable({listOfPokemon}: {listOfPokemon: Array<Pokemon>}) {   
+    const [db, setDb] = useState<Database|null>(null)
+
+    useEffect(() => {
+        const setup = async () => {
+            // const SQL = await initSqlJs()
+            const SQL = await initSqlJs({
+                // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
+                // You can omit locateFile completely when running in node
+                locateFile: file => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.10.3/${file}`
+            });
+            setDb(new SQL.Database())
+        }
+        setup()
+    }, [])
+
+    // Execute a single SQL string that contains multiple statements
+    let sqlstr = "CREATE TABLE IF NOT EXISTS hello (a int, b char); \
+    INSERT INTO hello VALUES (0, 'hello'); \
+    INSERT INTO hello VALUES (1, 'world');";
+    db?.run(sqlstr); // Run the query without returning anything
+
+    // Prepare an sql statement
+    const stmt = db?.prepare("SELECT * FROM hello WHERE a=:aval AND b=:bval");
+
+    // Bind values to the parameters and fetch the results of the query
+    const result = stmt?.getAsObject({':aval' : 1, ':bval' : 'world'});
+    console.log(result); // Will print {a:1, b:'world'}
+    const res = db?.exec("SELECT * FROM hello");
+    console.log(res)
+
     return (
         <Table sx={{maxWidth: "50vw"}}>
             {/* The labels for the table columns */}

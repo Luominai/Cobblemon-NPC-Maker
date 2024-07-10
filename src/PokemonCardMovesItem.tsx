690ac68272e4c4b71f65189e80bbff86edc26c51
KevinLi0711 <108951684+Luominai@users.noreply.github.com>
2024-07-10 14:01:51 -0400

    i finally understand sets and the spread operator

--- src/PokemonCardMovesItem.tsx
+++ src/PokemonCardMovesItem.tsx
@@ -3,12 +3,27 @@ import FilterContext from "./context/FilterContext"
 import { useContext } from "react"
 import {Moves} from "./data/showdown/moves"
 import itemsJson from "./data/items.json"
+import SelectedPokemonContext from "./context/SelectedPokemonContext"
 
 const listOfItems = Object.entries(itemsJson).map(([key, value]) => value)
 const listOfMoves = Object.entries(Moves).map(([key, value]) => value)
 
 function PokemonCardMovesItem() {
     const filters = useContext(FilterContext)
+    const selectedPokemon = useContext(SelectedPokemonContext)
+
+    let learnset = listOfMoves
+    if (selectedPokemon.selectedPokemon != null) {
+        const tm = selectedPokemon.selectedPokemon.moves.tm
+        const tutor = selectedPokemon.selectedPokemon.moves.tutor
+        const egg = selectedPokemon.selectedPokemon.moves.egg
+        const levelup = selectedPokemon.selectedPokemon.moves.levelup.map((move) => move.name)
+
+        // creates a new array of move names by spreading a set that is every array merged (this removes dupes), then maps each move name to its move
+        learnset = [... new Set([...tm, ...tutor])].map((moveName) => Moves[moveName]) 
+
+    }
+
     return (
         <Box display={"flex"} flexDirection={"column"} paddingLeft={"2px"} paddingRight={"2px"} width={"20%"}>
             {[0, 1, 2, 3].map((number) => {
@@ -37,7 +52,7 @@ function PokemonCardMovesItem() {
                     renderTags={(values) => 
                         values.map(value => value.name).join(' / ') // this renders the tags as a comma separated string rather than using chips
                     }
-                    options={listOfMoves}
+                    options={learnset}
                     getOptionLabel={(option) => option.name}
                     renderOption={(props, option) => {
                         const title = [option.category, option.type, option.basePower].join(" ")
